337ea84656710bb13b152feacf67cf2215603cc1
Jacksung <support@journal.com>
2025-07-17 17:54:38 +0700

    fix: force case-sensitive rename for Pages folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CustomerMenu from "./Pages/CustomerMenu";
-import AdminDashboard from "./Pages/AdminDashboard";
+import CustomerMenu from "./pages/CustomerMenu";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
