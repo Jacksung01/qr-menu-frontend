6e34861a5493ced5f5693588d7f1e3e6c35741c1
Jacksung <support@journal.com>
2025-07-17 17:59:31 +0700

    fix: correct Pages folder name to match case sensitivity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CustomerMenu from "./pages/CustomerMenu";
-import AdminDashboard from "./pages/AdminDashboard";
+import CustomerMenu from "./pages src/CustomerMenu";
+import AdminDashboard from "./pages src/AdminDashboard";
 
 function App() {
   return (
