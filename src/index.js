545daf8f9ba78c4271e651d9a84aad3926a5229d
Jacksung <support@journal.com>
2025-07-18 10:40:17 +0700

    fix: route and entry point for customer and admin pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Admin from './Admin';
+import CustomerMenu from './pages/CustomerMenu';
+import AdminDashboard from './pages/AdminDashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/" element={<CustomerMenu />} />
+      <Route path="/admin" element={<AdminDashboard />} />
     </Routes>
   </Router>
 );
